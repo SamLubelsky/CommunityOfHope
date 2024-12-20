e3ff57e8c34250888e4c620c9bbc4311d8a5f434
Sam Lubelsky <sammy56lt@gmail.com>
2024-12-19 19:05:44 -0600

    changes so project can deploy to firebase properly

--- backend/types/helpRequest.ts
+++
@@ -1,7 +0,0 @@
-export interface HelpRequest {
-  id: number;
-  mom_id: number;
-  volunteer_id?: number;
-  description: string;
-  active: boolean;
-}
--- coh-admin/config.ts
+++ coh-admin/config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://localhost:3000";
+export const BACKEND_URL = "https://api-v4j57qn4oq-uc.a.run.app";
