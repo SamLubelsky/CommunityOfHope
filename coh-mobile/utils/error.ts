6de61ef3ecc381565123e8199ea4270e41851e2d
Sam Lubelsky <sammy56lt@gmail.com>
2025-02-26 15:10:13 -0600

    fixed error page showing up

--- coh-mobile/utils/error.ts
+++ coh-mobile/utils/error.ts
@@ -1,9 +1,12 @@
 export const handleError = (throwError: any, responseData: any) => {
     if(throwError){
+        console.log("Handling erorr")
         if(responseData.message){   
+            console.log("error: ", responseData.message);
             throwError(new Error(responseData.message));    
         }
         if(responseData.error){
+            console.log("error: ", responseData.error);
             throwError(new Error(responseData.error));
         } else{
             throwError(new Error("An unknown error occurred"));
