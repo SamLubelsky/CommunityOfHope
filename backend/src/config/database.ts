339df43e58ee099ebeab9ae119136e28c4d49405
Sam Lubelsky <sammy56lt@gmail.com>
2025-04-17 22:59:43 -0500

    changed table schema

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -61,7 +61,7 @@ const createTables = async () => {
           emergency BOOLEAN DEFAULT FALSE,
           active BOOLEAN DEFAULT TRUE,
           dateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-          placeId INTEGER DEFAULT NULL,
+          placeId TEXT DEFAULT NULL,
           FOREIGN KEY (mom_id) REFERENCES users(id) ON DELETE SET NULL
           )
         `,[]);
